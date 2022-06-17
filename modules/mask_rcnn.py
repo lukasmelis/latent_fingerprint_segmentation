6db1a3e1bbe797486a468fa3695f61ef4b1811ef
Lukas <lukasmelis@dual>
2022-06-17 21:29:38 +0200

    feat: modify path to weights

--- modules/mask_rcnn.py
+++ modules/mask_rcnn.py
@@ -185,6 +185,7 @@ def create_image_paths(image_dir):
 
 
 def main(image_path):
+    start_time = time.time()
     inference_config = InferenceConfig()
 
     # Recreate the model in inference mode
@@ -195,7 +196,7 @@ def main(image_path):
     # Get path to saved weights
     # Either set a specific path or find last trained weights
     # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
-    model_path = "maskrcnn_latent/mask_rcnn_fingerprints_0046.h5"
+    model_path = "maskrcnn_latent/mask_rcnn_latent.h5"
 
     # Load trained weights (fill in path to trained weights here)
     assert model_path != "", "Provide path to trained weights"
@@ -204,7 +205,6 @@ def main(image_path):
 
     detection_results = []
     img = skimage.io.imread(image_path)
-    print(np.amax(img))
     if (np.amax(img) > 255):
         ratio = np.amax(img) / 256 
         img = ((img / ratio) - 1).astype('uint8')
@@ -212,8 +212,8 @@ def main(image_path):
     img_arr = np.array(img)
     results = model.detect([img_arr], verbose=1)
     r = results[0]
-    print(r['class_ids'])
-    print(image_path)
+    end_time = time.time()
+    print("cas Mask R-CNN(sec): ", round(end_time - start_time, 2))
     return visualize.display_instances(img, r['rois'], r['masks'], r['class_ids'],
                                 dataset_val.class_names, r['scores'], figsize=(5, 5))
 
